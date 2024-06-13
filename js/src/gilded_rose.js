feb26a8ea99eb99f3fc55db159f4cd9be859f8aa
Marin Rajkovic <marinrajkovic10@gmail.com>
2024-06-13 18:49:45 +0200

    Function added. Now moving to Testcases.

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -63,3 +63,19 @@ function update_conjured(item) {
     item.quality -= 2;
   }
 }
+
+function update_backstage_pass(item) {
+  if (item.quality < 50) {
+    item.quality += 1;
+    if (item.sell_in < 11 && item.quality < 50) {
+      item.quality += 1;
+    }
+    if (item.sell_in < 6 && item.quality < 50) {
+      item.quality += 1;
+    }
+  }
+  item.sell_in -= 1;
+  if (item.sell_in < 0) {
+    item.quality = 0;
+  }
+}
