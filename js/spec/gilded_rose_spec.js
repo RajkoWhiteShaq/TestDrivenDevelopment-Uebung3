b6b57a3dd0590064ffe3f4461b815631d0b59a26
Marin Rajkovic <marinrajkovic10@gmail.com>
2024-06-14 13:48:57 +0200

    Testcase 1 and 2 done. MOving to the next cases

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -1,9 +1,14 @@
 describe("Gilded Rose", function() {
 
-  it("should foo", function() {
-    items = [ new Item("foo", 0, 0) ];
+  it("should not allow quality to be more than 50", function() {
+    items = [ new Item("Aged Brie",2,  50) ];    
     update_quality();
-    expect(items[0].name).toEqual("fixme");
-  });
+    expect(items[0].quality).toBeLessThan(51);
+    });
 
-});
+    it("should maintain quality of Sulfuras at 80", function() {
+    items = [ new Item("Sulfuras, Hand of Ragnaros", 0, 80) ];
+    update_quality();
+    expect(items[0].quality).toEqual(80);
+    });
+}
