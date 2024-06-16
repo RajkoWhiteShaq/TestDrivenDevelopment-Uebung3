bf9e4bc706f6f44a3c47b5271615fe012df7afc0
Marin Rajkovic <marinrajkovic10@gmail.com>
2024-06-17 00:54:49 +0200

    Case 3 and 4 Done. Mobing to the last 5 Cases

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -1,14 +1,26 @@
 describe("Gilded Rose", function() {
 
-  it("should not allow quality to be more than 50", function() {
+  it("Quality over 50 is not allowed", function() {
     items = [ new Item("Aged Brie",2,  50) ];    
     update_quality();
     expect(items[0].quality).toBeLessThan(51);
     });
 
-    it("should maintain quality of Sulfuras at 80", function() {
+    it("Quality of Sulfuras should stay at 80", function() {
     items = [ new Item("Sulfuras, Hand of Ragnaros", 0, 80) ];
     update_quality();
     expect(items[0].quality).toEqual(80);
     });
-}
+
+    it("After sell-by date, the quality should degrade x2", function() {
+    items = [ new Item("foo", 0, 10) ];
+    update_quality();
+    expect(items[0].quality).toEqual(8); 
+    });
+
+    it("Quality can't be negative", function() {
+    items = [ new Item("foo", 0, 0) ];
+    update_quality();
+    expect(items[0].quality >= 0).toBeTruthy();
+    });
+
