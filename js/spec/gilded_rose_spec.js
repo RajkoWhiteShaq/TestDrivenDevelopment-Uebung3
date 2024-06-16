8ff3f1919160ee61b7bb8653e2605995e4d2e003
Marin Rajkovic <marinrajkovic10@gmail.com>
2024-06-17 00:58:01 +0200

    Finished every test case.

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -24,3 +24,33 @@ describe("Gilded Rose", function() {
     expect(items[0].quality >= 0).toBeTruthy();
     });
 
+
+    it(" Quality of Aged Brie should increase over time", function() {
+	    items = [ new Item("Aged Brie", 2, 10) ];
+	    update_quality();
+	    expect(items[0].quality).toEqual(11); 
+      });
+
+      it("If Backstage passes <= 10, increase Quality by 2'", function() {
+	    items = [ new Item("Backstage passes to a TAFKAL80ETC concert", 10, 10) ];
+	    update_quality();
+	    expect(items[0].quality).toEqual(12); 
+      });
+
+      it("If Backstage passes <= 5, increase Quality by 3'", function() {
+	    items = [ new Item("Backstage passes to a TAFKAL80ETC concert", 5, 10) ];
+	    update_quality();
+	    expect(items[0].quality).toEqual(13); 
+      });
+
+      it("If Backstage = 0, drop Quality to 0 '", function() {
+	    items = [ new Item("Backstage passes to a TAFKAL80ETC concert", 0, 10) ];
+	    update_quality();
+	    expect(items[0].quality).toEqual(0); 
+      });
+
+      it("Conjured items quality should fall twice as fast", function() {
+	    items = [ new Item("Conjured Mana Cake", 3, 6) ];
+	    update_quality();
+	    expect(items[0].quality).toEqual(4); });
+});
